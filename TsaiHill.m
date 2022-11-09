df92361a7b3b764f4236f6ef0ac421daa968f9b1
Wouter Grouve <w.j.b.grouve@utwente.nl>
2022-11-09 15:45:25 +0100

    Added templates for the failure criteria.

---
+++ TsaiHill.m
@@ -0,0 +1,41 @@
+function failed = TsaiHill(stress, strength)
+    % TsaiHill Returns True in case of failure.
+    %
+    % TH = sigma_1^2/S1^2 - sigma_1*sigma_2/S1^2 +
+    %      sigma_2^2/S2^2 + sigma_6^2/S6^2
+    %
+    % Failure occurs when TH > 1.
+    %
+    % Arguments:
+    %   stress   : Array of length 3 with the stress state in material CS.
+    %   strength : Matrix of size (3, 2) with the strength values. The first
+    %              row corresponds to the strength in 1 direction, the second
+    %              row to the strength in 2 direction, while the third row
+    %              represents the shear strength.
+    %              In case of normal strengths, the first column represents
+    %              the compressive strength and the second column represents
+    %              the tensile strength. For the shear strength, use a
+    %              negative value in the first column and a positive value
+    %              in the second.
+    %
+    % Returns:
+    %   failed   : Boolean is True in case of failure.
+    if stress(1) < 0 % check which strength component to use.
+        S1 = strength(1,1);
+    else
+        S1 = strength(1,2);
+    end
+    if stress(2) < 0
+        S2 = % complete statement
+    else
+        S2 = % complete statement
+    end
+    S3 = strength(3,1);
+
+    TH = % complete equation for Tsai-Hill value (see header of this function).
+    if TH > 1
+      failed = True;
+    else
+      failed = False;
+    end
+end
---
+++ max_stress_crit.m
@@ -0,0 +1,29 @@
+function failed = max_stress_crit(stress, strength)
+    % max_stress_crit Returns True in case of failure.
+    %
+    % Arguments:
+    %   stress   : Array of length 3 with the stress state in material CS.
+    %   strength : Matrix of size (3, 2) with the strength values. The first
+    %              row corresponds to the strength in 1 direction, the second
+    %              row to the strength in 2 direction, while the third row
+    %              represents the shear strength.
+    %              In case of normal strengths, the first column represents
+    %              the compressive strength and the second column represents
+    %              the tensile strength. For the shear strength, use a
+    %              negative value in the first column and a positive value
+    %              in the second.
+    %
+    % Returns:
+    %   failed   : Boolean indicating failure.
+    failed = False;
+
+    % check for each stress component whether this exceeds the strength value,
+    % return true if this is the case
+    if stress(1) < S(1,1) || stress(1) > S(1,2)  % first normal stress
+      failed = True;
+    elseif % complete this statement for the second normal stress
+      failed = True;
+    elseif % complete this statement for the shear stress
+      failed = True;
+    end
+end
